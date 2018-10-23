6c415230c49991bd35f4d20bbba3c27517e75549
jgascona <webcongas@gmail.com>
2018-10-23 14:08:24 +0200

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb://cafe-user:123456a@ds249128.mlab.com:49128/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLBD = urlBD;
